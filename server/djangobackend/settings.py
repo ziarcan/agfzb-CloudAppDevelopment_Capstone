f6b58216ac9796db9a05688e5aaf1e6e8bc8daa7
ziarcan <ziarcan@example.com>
2022-09-11 13:55:03 +0000

    deployed on cloud foundr

--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 APPEND_SLASH = True
 
-ALLOWED_HOSTS = ["localhost", "ziarcanhost.mybluemix.net"]
+ALLOWED_HOSTS = ["localhost", "ziarcan11-djangoapp.mybluemix.net"]
 
 
 # Application definition
